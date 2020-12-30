67279818bdc43b272258db8faed897b857b6f1db
hermes.mau <hermes.mau@gmail.com>
2020-12-30 17:27:29 -0100

    config

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -1,6 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import { Link } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import FloatingLabelInput from './FloatingLabelInput';
+
 var aboutUsData = {};
 
 
@@ -10,30 +12,66 @@ var aboutUsData = {};
 } */
 
 const AboutUsSection = (props: any) => {
-    const [aboutUs, setAboutUs] = useState("");
+  const [aboutUs, setAboutUs] = useState("");
 
-    useEffect(() => {
-        const AboutUsSection = {
-            aboutUs: '',
+  useEffect(() => {
+    const AboutUsSection = {
+      aboutUs: '',
     }
 
     AboutUsSection["aboutUs"] = aboutUs;
-    });
+  });
 
-    return (
-        <View >
-            <Text>TODO AboutUsSection </Text>
-            <FloatingLabelInput
-                value={aboutUs}
-                title='AboutUs'
-                onChangeText={(text: any) => {
-                    setAboutUs(text);
-                    props.parentUser(aboutUsData);
-                }}
-            />
-        </View>
-    );
-}
+  return (
+    <View >
+      <View >
+        <h2>About Us</h2>
+      </View >
+      <View  >
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+        <br></br>
+      </View>
+      <View >
+        <Text>*Ullamco laboris nisi ut aliquip ex ea commodo consequat</Text>
+        <Text>*Duis aute irure dolor in reprehenderit in voluptate velit</Text>
+        <Text >*Ullamco laboris nisi ut aliquip ex ea commodo consequa</Text>
+        <br></br>
+      </View>
+      <View >
+        <Text> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+      </View>
+    </View >
+  );
+};
+const styles = StyleSheet.create({
+
+  h1: {
+    color: '#000000',
+    fontSize: 20,
+  },
+
+  h2: {
+    color: '#FAE042',
+    fontSize: 10,
+    marginTop: 4,
+  },
+
+  image: {
+    width: 200,
+    height: 150,
+    justifyContent: 'center',
+  },
+  container: {
+    flex:1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start' // if you want to fill rows left to right
+  },
+  item: {
+    width: '50%' // is 50% of container width
+  }
+
+});
 
 /* AboutUsSection.defaultProps = {
     menu: [{ name: 'Angola', component: Home() },
--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,40 +1,82 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { ListItem, Icon, Input } from 'react-native-elements';
 import FloatingLabelInput from './FloatingLabelInput';
 var contactSectionData = {};
 
 
-/* type HeaderProps = {
-    menu: [{ name: String, component: Function},
-    ]
-} */
 
 const ContactSection = (props: any) => {
-    const [contactSection, setContactSection] = useState("");
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [subjet, setSubjet] = useState(null);
+    const [message, setMessage] = useState(null);
+
 
     useEffect(() => {
         const ContactSection = {
             contactSection: '',
-    }
+        }
 
-    ContactSection["contactSection"] = contactSection;
+        ContactSection["ContactSection"] = ContactSection;
     });
 
 
     return (
         <View >
-            <Text>TODO ContactSection </Text>
-            <FloatingLabelInput
-                value={contactSection}
-                title='ContactSection'
-                onChangeText={(text: any) => {
-                    setContactSection(text);
-                    props.parentUser(contactSectionData);
-                }}
+            <View ><h2>ContactUs </h2></View>
+            <view><Text>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</Text>
+            </view>
+            <br></br>
+            <Text >A108 Adam Street </Text>
+            <Text >New York, NY 535022</Text>
+            <Text >info@example.com</Text>
+            <Text >+1 5589 55488 55s</Text>
+
+            <Input
+                ///  style={styles.textArea}
+                underlineColorAndroid="transparent"
+                placeholder="Your Name"
+                placeholderTextColor="grey"
+                numberOfLines={1}
+                multiline={true}
+                onChangeText={text => setName(text)}
             />
+            <Input
+                ///  style={styles.textArea}
+                underlineColorAndroid="transparent"
+                placeholder="Your Email"
+                placeholderTextColor="grey"
+                numberOfLines={1}
+                multiline={true}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                ///  style={styles.textArea}
+                underlineColorAndroid="transparent"
+                placeholder="Subject"
+                placeholderTextColor="grey"
+                numberOfLines={1}
+                multiline={true}
+                onChangeText={text => setSubjet(text)}
+            />
+            <Input
+                ///  style={styles.textArea}
+                underlineColorAndroid="transparent"
+                placeholder="Message"
+                placeholderTextColor="grey"
+                numberOfLines={1}
+                multiline={true}
+                onChangeText={text => setMessage(text)}
+            />
+            <Text >Loading</Text>
+            <Text >Your message has been sent. Thank you!</Text>
+
+
         </View>
     );
 }
+
 
 /* Header.defaultProps = {
     menu: [{ name: 'Angola', component: Home() },
