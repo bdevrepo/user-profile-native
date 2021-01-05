1ce2b2a34091b1f2ab45d2a41341f3f38ad6019c
hermes.mau <hermes.mau@gmail.com>
2021-01-05 19:08:09 -0100

    Conteudo

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
 import FloatingLabelInput from './FloatingLabelInput';
 var faqSectionData = {};
- 
+
 
 
 /* type HeaderProps = {
@@ -16,22 +16,34 @@ const FAQSection = (props: any) => {
     useEffect(() => {
         const FAQSection = {
             faqSection: '',
-    }
+        }
 
-    FAQSection["faqSection"] = faqSection;
+        FAQSection["faqSection"] = faqSection;
     });
 
     return (
         <View >
-            <Text>TODO FAQSection </Text>
-            <FloatingLabelInput
-                value={faqSection}
-                title='FAQ Section'
-                onChangeText={(text: any) => {
-                    setFaqSection(text);
-                    props.parentUser(faqSectionData);
-                }}
-            />
+            <h2>Frequently Asked Questions</h2>
+            <h4>Non consectetur a erat nam at lectus urna duis?</h4>
+            <p>
+                Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+            </p>
+            <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+            <p>
+                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
+            </p>
+            <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+            <p>
+                Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
+            </p>
+            <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
+            <p>
+                Aperiam itaque sit optio et deleniti eos nihil quidem cumque. Voluptas dolorum accusantium sunt sit enim. Provident consequuntur quam aut reiciendis qui rerum dolorem sit odio. Repellat assumenda soluta sunt pariatur error doloribus fuga.
+            </p>
+            <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
+            <p>
+                Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+            </p>
         </View>
     );
 }
