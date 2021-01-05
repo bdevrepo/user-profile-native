33f68a0b3717389b1c1bb01db4cb9db3e01354ec
hermes.mau <hermes.mau@gmail.com>
2021-01-05 17:49:05 -0100

    container

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -24,14 +24,16 @@ const ServicesSection = (props: any) => {
     return (
         <View >
             <Text>TODO ServicesSection </Text>
-            <FloatingLabelInput
-                value={servSection}
-                title='ServicesSection'
-                onChangeText={(text: any) => {
-                    setservSection(text);
-                    props.parentUser(servSectionData);
-                }}
-            />
+            <h2>Services</h2>
+            <Text>Magnam dolores commodi suscipit eius consequatur ex aliquid fug</Text>
+            <h4>Lorem Ipsum</h4>
+            <Text>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi </Text>
+            <h4>Sed ut perspiciatis</h4>
+            <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </Text>
+            <h4>Magni Dolore</h4>
+            <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia </Text>
+            <h4>Nemo Enim</h4>
+            <Text>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis </Text>
         </View>
     );
 }
